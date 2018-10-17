62f61167a6279eb4d51aaa3820919a52c7dc4cc8
alikurniawan <alikoerniawan02@gmail.com>
2018-10-17 18:22:02 +0700

    ReactCRAB-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
+import './assets/css/index.css'
+import Home from './assets/jsx/pages/home';
+
+// own
+import { Container } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Container>
+          <body>
+            <div>
+              <Router>
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                {/* <Route path="/about" component={About} /> */}
+              </Switch>
+              </Router>
+            
+            </div>
+          </body>
+        
+        </Container>
+          
+        
+        
       </div>
     );
   }
