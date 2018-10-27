4fcd8e155e24805fa42b7b846b804ed77c72d365
alikurniawan <alikoernaiwan02@gmail.com>
2018-10-27 19:49:39 +0700

    Movies

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -16,6 +16,7 @@ class Home extends Component {
         return (
             <div>
                 <PageControll>
+                    {/* //test */}
                     <EmbedMov />
                 </PageControll>  
             </div>
